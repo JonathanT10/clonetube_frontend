73243f680baedc39d9cddfca3284012a9f2bab74
Jonathan Terrell <jonathan.terrell@stewartorg.com>
2021-05-14 09:21:57 -0500

    remove youtube api key

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ const [info, setInfo] = useState([]);
 
 
     useEffect(()=> {
-        axios.get( "https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=Ks-_Mh1QhMc&type=video&key=AIzaSyAGnWKXtaDiprjEzk4jg-sFlIL_Vz9g36M")
+        axios.get( "https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=")
             .then(response => setGroup(response.data[props.index])
              )}, [group, props.index]);
 
