16ec6c2f09d25e75668957d159735c6b0332928a
Jonathan Terrell <jonathan.terrell@stewartorg.com>
2021-05-13 19:22:59 -0500

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
+
+function App() {
+
+
+    useEffect(()=> {
+        axios
+        .get()
+        .then()
+    })
+    return(
+
+    );
+}
+
+export default App;
