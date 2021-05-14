004e00c2f3e38c7c46fca6447638036432d05b62
Jonathan Terrell <jonathan.terrell@stewartorg.com>
2021-05-13 21:18:57 -0500

    app.jsx added axios get to youtube api

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import axios from 'axios';
-import {useState, useEffect} from 'react';
 
-
-function App() {
+const Container = (props) =>{
+const [group, setGroup] = useState({});
+const [info, setInfo] = useState([]);
 
 
     useEffect(()=> {
-        axios
-        .get()
-        .then()
-    })
-    return(
+        axios.get( "https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=Ks-_Mh1QhMc&type=video&key=AIzaSyAGnWKXtaDiprjEzk4jg-sFlIL_Vz9g36M")
+            .then(response => setGroup(response.data[props.index])
+             )}, [group, props.index]);
 
+    return(
+        <div>
+            <h1>{group}</h1>
+        </div>
     );
 }
 
-export default App;
+export default Container;
